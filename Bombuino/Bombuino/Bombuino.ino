776a109696cd786c7a83610cdf222659b09efb3c
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-06-16 15:06:40 +0200

    ajout nouveau bombuino

---
+++ Bombuino/Bombuino/Bombuino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Bombuino.ino
+ Created:	16/06/2021 15:04:59
+ Author:	fabrice ferrere
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Bombuino/Bombuino/Entity.h
@@ -0,0 +1,14 @@
+// Entity.h
+
+#ifndef _ENTITY_h
+#define _ENTITY_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
