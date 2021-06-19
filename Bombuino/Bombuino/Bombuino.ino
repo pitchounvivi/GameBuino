bb606fa451560033400f0db07a2c14e8ae1b4845
GET MOUSSED <mofanafoussa@gmail.com>
2021-06-19 19:25:31 +0200

    added all sprites and messing around with importing them in game

--- Bombuino/Bombuino/Bombuino.ino
+++ Bombuino/Bombuino/Bombuino.ino
@@ -7,12 +7,27 @@
 #include "Entity.h"
 #include <Gamebuino-Meta.h>
 #include <cstdlib>
+#include "Ressources.h"
 
 void setup() {
-
+	gb.begin();
 }
 
+Image robot(Ressources::ROBOT_TEXTURE);
+Image woman(Ressources::WOMAN_TEXTURE);
+Image dog(Ressources::DOG_TEXTURE);
+Image alien(Ressources::ALIEN_TEXTURE);
+Image walls(Ressources::WALLS_TEXTURE);
+
+Image* test[] = { &robot , &woman, &dog, &alien };
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	while (!gb.update());
+	gb.display.clear(GRAY);
+	for (int i = 0; i < 4; i += 1) {
+		for (uint8_t x = 0; x < 6; x += 1) {
+			test[i]->setFrame(x);
+			gb.display.drawImage(x * 7, i * (6 + 5), *test[i]);
+		}
+	}
 }
